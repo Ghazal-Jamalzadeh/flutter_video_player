7626cae16eb9bf0ccb5165137b0772eb0e04ad77
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-08-11 15:57:06 +0430

    extract control panel widget

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -31,12 +31,6 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
 
-    final Timer timer = Timer.periodic(Duration(milliseconds: 200), (timer) {
-      setState(() {
-
-      });
-
-    }) ;
 
     return SafeArea(
       child: Scaffold(
@@ -53,89 +47,118 @@ class _MainScreenState extends State<MainScreen> {
                       width: double.infinity,
                       height: 300,
                       child: VideoPlayer(widget.controller))),
-              Positioned.fill(
-                  child: Container(
-                decoration: BoxDecoration(color: Colors.black.withOpacity(0.2)),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        ClipRRect(
-                            borderRadius: BorderRadius.circular(24.0),
-                            child: Image.asset(
-                              'assets/shark.jpg',
-                              width: 54,
-                              height: 54,
-                              fit: BoxFit.fill,
-                            )),
-                        const SizedBox(
-                          width: 16,
-                        ),
-                        const Text(
-                          'Sharky',
-                          style: TextStyle(color: Colors.white),
-                        )
-                      ],
-                    ),
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text('The Earth',
-                              style: TextStyle(color: Colors.white)),
-                          const SizedBox(
-                            height: 4,
-                          ),
-                          const Text('پنجشنبه ۱۹ مرداد',
-                              style: TextStyle(color: Colors.white)),
-                          const SizedBox(
-                            height: 4,
-                          ),
-                          const Text('به یاد پویان',
-                              style: TextStyle(color: Colors.white)),
-                          const SizedBox(
-                            height: 4,
-                          ),
-                          /*
-                      * allowScrubbing :به کاربر اجازه میده با کلیک روی پروگرس بار جابه جا شه
-                      * */
-                          VideoProgressIndicator(
-                            widget.controller,
-                            allowScrubbing: true,
-                            colors:  VideoProgressColors(
-                                playedColor: Colors.white,
-                                backgroundColor: Colors.white.withOpacity(0.5)),
-                          ),
-                          const SizedBox(
-                            height: 4,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(widget.controller.value.position.toMinutesSeconds(),
-                                  style: TextStyle(color: Colors.white)),
-                              Text(widget.controller.value.duration.toMinutesSeconds(),
-                                  style: TextStyle(color: Colors.white)),
-                            ],
-                          ) ,
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              IconButton(onPressed: (){}, iconSize: 32 , icon: const Icon(CupertinoIcons.backward_fill , color: Colors.white,)),
-                              IconButton(onPressed: (){}, iconSize: 56 , icon:  Icon(widget.controller.value.isPlaying ? CupertinoIcons.pause_circle_fill : CupertinoIcons.play_circle_fill , color: Colors.white,)),
-                              IconButton(onPressed: (){}, iconSize : 32  , icon: const Icon(CupertinoIcons.forward_fill , color: Colors.white,)),
-
-                          ],)
-                        ])
-                  ],
-                ),
-              ))
+              VideoControllPanel( controller: widget.controller)
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class VideoControllPanel extends StatefulWidget {
+
+  VideoPlayerController controller;
+
+  VideoControllPanel({
+    Key? key,
+ required this.controller,
+  }) : super(key: key);
+
+  @override
+  State<VideoControllPanel> createState() => _VideoControllPanelState();
+}
+
+class _VideoControllPanelState extends State<VideoControllPanel> {
+
+  @override
+  Widget build(BuildContext context) {
+
+     final Timer timer = Timer.periodic(Duration(milliseconds: 200), (timer) {
+      setState(() {
+
+      });
+
+    }) ;
+
+    return Positioned.fill(
+        child: Container(
+      decoration: BoxDecoration(color: Colors.black.withOpacity(0.2)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(24.0),
+                  child: Image.asset(
+                    'assets/shark.jpg',
+                    width: 54,
+                    height: 54,
+                    fit: BoxFit.fill,
+                  )),
+              const SizedBox(
+                width: 16,
+              ),
+              const Text(
+                'Sharky',
+                style: TextStyle(color: Colors.white),
+              )
+            ],
+          ),
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('The Earth',
+                    style: TextStyle(color: Colors.white)),
+                const SizedBox(
+                  height: 4,
+                ),
+                const Text('پنجشنبه ۱۹ مرداد',
+                    style: TextStyle(color: Colors.white)),
+                const SizedBox(
+                  height: 4,
+                ),
+                const Text('به یاد پویان',
+                    style: TextStyle(color: Colors.white)),
+                const SizedBox(
+                  height: 4,
+                ),
+                /*
+            * allowScrubbing :به کاربر اجازه میده با کلیک روی پروگرس بار جابه جا شه
+            * */
+                VideoProgressIndicator(
+                  widget.controller,
+                  allowScrubbing: true,
+                  colors:  VideoProgressColors(
+                      playedColor: Colors.white,
+                      backgroundColor: Colors.white.withOpacity(0.5)),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(widget.controller.value.position.toMinutesSeconds(),
+                        style: TextStyle(color: Colors.white)),
+                    Text(widget.controller.value.duration.toMinutesSeconds(),
+                        style: TextStyle(color: Colors.white)),
+                  ],
+                ) ,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconButton(onPressed: (){}, iconSize: 32 , icon: const Icon(CupertinoIcons.backward_fill , color: Colors.white,)),
+                    IconButton(onPressed: (){}, iconSize: 56 , icon:  Icon(widget.controller.value.isPlaying ? CupertinoIcons.pause_circle_fill : CupertinoIcons.play_circle_fill , color: Colors.white,)),
+                    IconButton(onPressed: (){}, iconSize : 32  , icon: const Icon(CupertinoIcons.forward_fill , color: Colors.white,)),
+
+                ],)
+              ])
+        ],
+      ),
+    ));
   }
 }
