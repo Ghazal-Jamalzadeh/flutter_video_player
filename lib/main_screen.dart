1158183cbd2b22d9f15664d40f0129c6ee4100b4
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-08-11 16:30:03 +0430

    added gesture detector

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -14,24 +14,24 @@ class MainScreen extends StatefulWidget {
         ..setLooping(true)
         ..play();
 
+  bool showControlPanel = false;
+
+  Timer? timer;
+
   @override
   State<MainScreen> createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
-  
-
-  
   @override
   void dispose() {
     widget.controller.dispose();
+    widget.timer?.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-
-
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.black,
@@ -43,26 +43,58 @@ class _MainScreenState extends State<MainScreen> {
                   top: 32,
                   right: 0,
                   left: 0,
-                  child: SizedBox(
-                      width: double.infinity,
-                      height: 300,
-                      child: VideoPlayer(widget.controller))),
-              VideoControllPanel( controller: widget.controller)
+                  /*
+                  * Inkwell vs GestureDetector :
+                  * اینکول یک ریپل افکتی داره که بعضی جاها جالب نیست.
+                  * */
+                  child: GestureDetector(
+                    onTap: () {
+                      if (!widget.showControlPanel) {
+                        setState(() {
+                          widget.showControlPanel = true;
+                        });
+                        initControlPanelTimer();
+                      }
+                    },
+                    child: SizedBox(
+                        width: double.infinity,
+                        height: 300,
+                        child: VideoPlayer(widget.controller)),
+                  )),
+              if (widget.showControlPanel)
+                GestureDetector(
+                    onTap: () {
+                      if (widget.showControlPanel) {
+                        setState(() {
+                          widget.showControlPanel = false;
+                        });
+                        widget.timer?.cancel();
+                      }
+                    },
+                    child: VideoControllPanel(controller: widget.controller))
             ],
           ),
         ),
       ),
     );
   }
+
+  void initControlPanelTimer() {
+    widget.timer?.cancel();
+    widget.timer = Timer(const Duration(seconds: 5), () {
+      setState(() {
+        widget.showControlPanel = false;
+      });
+    });
+  }
 }
 
 class VideoControllPanel extends StatefulWidget {
-
   VideoPlayerController controller;
 
   VideoControllPanel({
     Key? key,
- required this.controller,
+    required this.controller,
   }) : super(key: key);
 
   @override
@@ -70,16 +102,11 @@ class VideoControllPanel extends StatefulWidget {
 }
 
 class _VideoControllPanelState extends State<VideoControllPanel> {
-
   @override
   Widget build(BuildContext context) {
-
-     final Timer timer = Timer.periodic(Duration(milliseconds: 200), (timer) {
-      setState(() {
-
-      });
-
-    }) ;
+    final Timer timer = Timer.periodic(Duration(milliseconds: 200), (timer) {
+      setState(() {});
+    });
 
     return Positioned.fill(
         child: Container(
@@ -107,56 +134,72 @@ class _VideoControllPanelState extends State<VideoControllPanel> {
               )
             ],
           ),
-          Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('The Earth',
-                    style: TextStyle(color: Colors.white)),
-                const SizedBox(
-                  height: 4,
-                ),
-                const Text('پنجشنبه ۱۹ مرداد',
-                    style: TextStyle(color: Colors.white)),
-                const SizedBox(
-                  height: 4,
-                ),
-                const Text('به یاد پویان',
-                    style: TextStyle(color: Colors.white)),
-                const SizedBox(
-                  height: 4,
-                ),
-                /*
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            const Text('The Earth', style: TextStyle(color: Colors.white)),
+            const SizedBox(
+              height: 4,
+            ),
+            const Text('پنجشنبه ۱۹ مرداد',
+                style: TextStyle(color: Colors.white)),
+            const SizedBox(
+              height: 4,
+            ),
+            const Text('به یاد پویان', style: TextStyle(color: Colors.white)),
+            const SizedBox(
+              height: 4,
+            ),
+            /*
             * allowScrubbing :به کاربر اجازه میده با کلیک روی پروگرس بار جابه جا شه
             * */
-                VideoProgressIndicator(
-                  widget.controller,
-                  allowScrubbing: true,
-                  colors:  VideoProgressColors(
-                      playedColor: Colors.white,
-                      backgroundColor: Colors.white.withOpacity(0.5)),
-                ),
-                const SizedBox(
-                  height: 4,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(widget.controller.value.position.toMinutesSeconds(),
-                        style: TextStyle(color: Colors.white)),
-                    Text(widget.controller.value.duration.toMinutesSeconds(),
-                        style: TextStyle(color: Colors.white)),
-                  ],
-                ) ,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    IconButton(onPressed: (){}, iconSize: 32 , icon: const Icon(CupertinoIcons.backward_fill , color: Colors.white,)),
-                    IconButton(onPressed: (){}, iconSize: 56 , icon:  Icon(widget.controller.value.isPlaying ? CupertinoIcons.pause_circle_fill : CupertinoIcons.play_circle_fill , color: Colors.white,)),
-                    IconButton(onPressed: (){}, iconSize : 32  , icon: const Icon(CupertinoIcons.forward_fill , color: Colors.white,)),
-
-                ],)
-              ])
+            VideoProgressIndicator(
+              widget.controller,
+              allowScrubbing: true,
+              colors: VideoProgressColors(
+                  playedColor: Colors.white,
+                  backgroundColor: Colors.white.withOpacity(0.5)),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(widget.controller.value.position.toMinutesSeconds(),
+                    style: TextStyle(color: Colors.white)),
+                Text(widget.controller.value.duration.toMinutesSeconds(),
+                    style: TextStyle(color: Colors.white)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    iconSize: 32,
+                    icon: const Icon(
+                      CupertinoIcons.backward_fill,
+                      color: Colors.white,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    iconSize: 56,
+                    icon: Icon(
+                      widget.controller.value.isPlaying
+                          ? CupertinoIcons.pause_circle_fill
+                          : CupertinoIcons.play_circle_fill,
+                      color: Colors.white,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    iconSize: 32,
+                    icon: const Icon(
+                      CupertinoIcons.forward_fill,
+                      color: Colors.white,
+                    )),
+              ],
+            )
+          ])
         ],
       ),
     ));
